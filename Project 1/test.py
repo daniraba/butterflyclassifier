2f3d0af19a00ad4a17e309fc5246103b3bafb656
GBThunder <90589012+GBThunder@users.noreply.github.com>
2024-06-11 11:28:31 -0400

    Update test.py

---
+++ Project 1/test.py
@@ -0,0 +1,52 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+from PIL import Image
+from INeuralNetwork import INeuralNetwork
+
+n=INeuralNetwork()
+n.load_state_dict(torch.load("Ins.pth"))
+
+
+
+def preprocess(f):
+    image=Image.open(f)
+    image=image.resize((50,50))
+    #print(image.size)
+    a=np.array(image)/255.0
+    #print(a.shape)
+    a=a.reshape(50*50*3)
+    return a
+
+test="Project 1/Test/"
+types=["Butterfly Test","Grasshopper Test","Ladybug Test","Dragonfly Test","Mosquito Test"]
+
+correct = 0
+
+for i in range(len(types)):
+    directory=test+types[i]
+    c = 0
+    for filename in os.listdir(directory):
+        f= os.path.join(directory,filename)
+        print(i, f, c)
+        try:
+            if os.path.isfile(f):
+                img=preprocess(f)
+                label=i
+        except:
+            print('Skipped')
+            continue
+    
+        target=np.zeros(len(types))
+        target[label]=1.0
+        output=n.forward(img).detach.numpy()
+
+
+        c += 1
+        if c == 100:
+            guess=np.argmax(output)
+            if guess==label:
+                correct+=1
+        print("Accuracy:",correct/100)
+        break
